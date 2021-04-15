c2977da8957165ca2f107b7c4e10131bd8a20462
Yaroslav Kostyannikov <ykostyannikov@lohika.com>
2021-04-15 15:29:36 +0300

    final version of the app

--- introduction/app/app.go
+++ introduction/app/app.go
@@ -5,20 +5,8 @@ import (
 )
 
 func main() {
-	customers := getData()
-	fmt.Println(customers)
-	fmt.Println(len(customers))
-}
-
-func getData() (customers []string) {
-	customers = []string{"killabayte", "The Mad Hatter"}
-	customers = append(customers, "Alice")
-	customers = append(customers, "March Hare")
-	customers = append(customers, "Dormouse")
-
+	customers := GetCustomers()
 	for _, customer := range customers {
 		fmt.Println(customer)
 	}
-
-	return customers
 }
